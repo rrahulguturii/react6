405254691916d4a1915c6ec80465019432619875
rrahulguturii <rrahulguturii@gmail.com>
2023-10-14 22:49:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './header';
+import About from './about';
+import Projects from './projects';
+import Contact from './contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
